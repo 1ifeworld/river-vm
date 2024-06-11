dbc8a51cf45d5dbdf953c8640c63cea3a183d745
Salief <salief@Maxs-Mac-mini.lan>
2024-06-11 13:40:01 -0400

    wip apollo server instance

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { buildSchema } from 'drizzle-graphql'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { db } from './db.js';
+
+const { schema } = buildSchema(db)
+
+const server = new ApolloServer({ schema })
+const { url } = await startStandaloneServer(server)
+
+console.log(`Server ready at ${url}`)
