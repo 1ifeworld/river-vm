fd44722768899976885c1e86069959d3b80015a7
Salief <salief@Maxs-Mac-mini.lan>
2024-06-16 11:54:37 -0400

    update apollo server config

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ import { db } from './db.js'
 const { schema } = buildSchema(db)
 
 const server = new ApolloServer({ schema })
-const { url } = await startStandaloneServer(server)
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: 4000,
+  },
+})
 
 console.log(`Server ready at ${url}`)
